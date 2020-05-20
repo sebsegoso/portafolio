ac0c97a800cd764616191529f0e5cb0b3c719cc3
Sebastián Segura Osorio <sseguraosorio@gmail.com>
2020-05-20 00:25:57 -0400

    fix tablet mode / edit carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,15 @@ $(document).ready(function () {
   // agrega smooth scrolling a todos los enlaces "a"
   $("a").on('click', function (event) {
 
-    // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       event.preventDefault();
 
-      // Store hash
       var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function () {
 
-        // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
     }
@@ -26,10 +21,7 @@ $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
   })
 
-  // cambia padding de la barra al desaparecer
-  $('body').css('padding-top', $('.navbar').outerHeight() + 'px')
-
-// detector de scroll
+// Ocultar navbar al bajar
 if ($('.smart-scroll').length > 0) {
     var last_scroll_top = 0;
     $(window).on('scroll', function() {
